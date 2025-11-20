fc9b0dff7ad5a0b0bc9746eeb0bc9f161d1c895c
devsha256 <dev.hsaddam@gmail.com>
2025-11-21 01:45:32 +0530

    Add mule implementation

--- src/main/java/io/github/devsha256/saprfctest/controller/RfcMetadataController.java
+++
@@ -1,5 +0,0 @@
-package io.github.devsha256.saprfctest.controller;
-
-public class RfcMetadataController {
-    
-}
--- src/main/java/io/github/devsha256/saprfctest/model/RfcTable.java
+++
@@ -1,5 +0,0 @@
-package io.github.devsha256.saprfctest.model;
-
-public class RfcTable {
-    
-}
